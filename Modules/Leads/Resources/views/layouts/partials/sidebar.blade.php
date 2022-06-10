75804d28045df572d205e027b4a0a28bef37811c
SalvatorePortera <106048648+SalvatorePortera@users.noreply.github.com>
2022-06-10 16:05:23 -0400

    fasd
    fasdfsadf

--- Modules/Leads/Resources/views/layouts/partials/sidebar.blade.php
+++ Modules/Leads/Resources/views/layouts/partials/sidebar.blade.php
@@ -1,41 +1,39 @@
-<li class="treeview {{ in_array($request->segment(1), ['leads']) ? 'active active-sub' : '' }}">
-    <a href="#"><i class="fa fa-lg fa-lightbulb-o"></i> <span>@lang('leads::lang.leads')</span>
-        <span class="pull-right-container">
-            <i class="right fas fa-angle-left"></i>
-        </span>
+<li class="nav-item treeview {{ in_array($request->segment(1), ['leads']) ? 'active active-sub' : '' }}">
+    <a href="#"><i class="fa fa-lg fa-lightbulb-o nav-icon"></i> <p>@lang('leads::lang.leads') <i class="right fas fa-angle-left"></i></p>
+        
     </a>
-    <ul class="treeview-menu">
+    <ul class="nav nav-treeview">
         @if($leads)
         @if(auth()->user()->can('leads.view') || auth()->user()->can('leads.edit') ||
         auth()->user()->can('leads.destory')|| auth()->user()->can('leads.create'))
-        <li class="{{ $request->segment(1) == 'leads' && $request->segment(2) == 'leads'? 'active' : '' }}">
-            <a href="{{action('\Modules\Leads\Http\Controllers\LeadsController@index')}}"><i
-                    class="fa fa-lightbulb-o"></i>@lang('leads::lang.leads')</a>
+        <li class="nav-item {{ $request->segment(1) == 'leads' && $request->segment(2) == 'leads'? 'active' : '' }}">
+            <a href="{{action('\Modules\Leads\Http\Controllers\LeadsController@index')}}" class="nav-link"><i
+                    class="fa fa-lightbulb-o nav-icon"></i> <p>@lang('leads::lang.leads')</p></a>
         </li>
         @endcan
         @endif
         @if($leads_import)
         @can('leads.import')
-        <li class="{{ $request->segment(1) == 'leads' && $request->segment(2) == 'import'? 'active' : '' }}">
-            <a href="{{action('\Modules\Leads\Http\Controllers\ImportLeadsController@index')}}"><i
-                    class="fa fa-download"></i>@lang('leads::lang.import_data')</a>
+        <li class="nav-item {{ $request->segment(1) == 'leads' && $request->segment(2) == 'import'? 'active' : '' }}">
+            <a href="{{action('\Modules\Leads\Http\Controllers\ImportLeadsController@index')}}" class="nav-link"><i
+                    class="fa fa-download nav-icon"></i><p>@lang('leads::lang.import_data')</p></a>
         </li>
         @endcan
         @endif
         @if($day_count)
         @can('day_count')
-        <li class="{{ $request->segment(1) == 'leads' && $request->segment(2) == 'day-count'? 'active' : '' }}">
-            <a href="{{action('\Modules\Leads\Http\Controllers\DayCountController@index')}}"><i
-                    class="fa fa-plus"></i>@lang('leads::lang.day_count')</a>
+        <li class="nav-item {{ $request->segment(1) == 'leads' && $request->segment(2) == 'day-count'? 'active' : '' }}">
+            <a href="{{action('\Modules\Leads\Http\Controllers\DayCountController@index')}}" class="nav-link"><i
+                    class="fa fa-plus nav-icon"></i><p>@lang('leads::lang.day_count')</p></a>
         </li>
         @endcan
         @endif
 
         @if($leads_settings)
         @can('leads.settings')
-        <li class="{{ $request->segment(1) == 'leads' && $request->segment(2) == 'settings'? 'active' : '' }}">
-            <a href="{{action('\Modules\Leads\Http\Controllers\SettingController@index')}}"><i
-                    class="fa fa-cogs"></i>@lang('leads::lang.settings')</a>
+        <li class="nav-item {{ $request->segment(1) == 'leads' && $request->segment(2) == 'settings'? 'active' : '' }}">
+            <a href="{{action('\Modules\Leads\Http\Controllers\SettingController@index')}}" class="nav-link"><i
+                    class="fa fa-cogs nav-icon"></i><p>@lang('leads::lang.settings')</p></a>
         </li>
         @endcan
         @endif
